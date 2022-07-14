aee70aff673393829905194ec95e1e869fe5e56e
Everton <everton.leice@yahoo.com.br>
2022-07-14 15:14:11 -0300

    Expert Talk Projeto

---
+++ src/one/digitalinnovation/gof/singleton/SingletonEager.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.gof.singleton;
+
+/**
+ * Singleton "Apressado".
+ * 
+ * @author Everton
+ */
+public class SingletonEager {
+
+	private static SingletonEager instancia = new SingletonEager ();
+	
+	private SingletonEager() {
+		super();
+	}
+	
+	public static SingletonEager getInstancia() {
+		return instancia;
+	}
+}
